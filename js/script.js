73b14886df9d13b85cbba0f8f42642ac0b794d94
Ahmed Hatem <ugs.1331@ci.suez.edu.eg>
2023-12-19 22:21:47 +0200

    Valdation of login page using javascript and css

--- js/script.js
+++ js/script.js
@@ -77,3 +77,113 @@ allButtons.forEach((button) => {
 
 
 //////////////////////////login form validation//////////////////////////
+let SignUpForm = document.getElementById("up-form");
+let SignInForm = document.getElementById("in-form");
+let userName = document.getElementById("user-name");
+let SignUpEmail = document.getElementById("up-email");
+let SignUpPassword = document.getElementById("up-password");
+let SignInEmail = document.getElementById("in-email");
+let SignInPassword = document.getElementById("in-password");
+
+let EmailError = document.getElementById("email-error");
+let PasswordError = document.getElementById("password-error");
+
+let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+let passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[_])[a-zA-Z0-9_]+$/;
+
+let setError = (input, message) => {
+    let inputControl = input.parentElement;
+    let errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add("error");
+    inputControl.classList.remove("success");
+};
+
+let setSuccess = (input) => {
+    let inputControl = input.parentElement;
+    let errorDisplay = inputControl.querySelector(".error");
+
+    errorDisplay.innerText = "";
+    inputControl.classList.add("success");
+    inputControl.classList.remove("error");
+};
+
+// sign up form validation
+
+SignUpForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkSignInInputs();
+});
+
+
+let checkSignInInputs = () => {
+    let UserNameValue = userName.value.trim();
+    let SignUpEmailValue = SignUpEmail.value.trim();
+    let SignUpPasswordValue = SignUpPassword.value.trim();
+
+    if (UserNameValue === "") {
+        setError(userName, "User Name cannot be blank");
+    } else {
+        setSuccess(userName);
+    }
+
+    if (SignUpEmailValue === "") {
+        setError(SignUpEmail, "Email cannot be blank");
+    } else if (!emailRegex.test(SignUpEmailValue)) {
+        setError(SignUpEmail, "Email is not valid");
+    } else {
+        setSuccess(SignUpEmail);
+    }
+
+    if (SignUpPasswordValue === "") {
+        setError(SignUpPassword, "Password cannot be blank");
+    } else if (!passwordRegex.test(SignUpPasswordValue)) {
+        setError(SignUpPassword, "Password is not valid");
+    } else if (SignUpPasswordValue.length < 8) {
+        setError(SignUpPassword, "Password must be 8 characters long");
+    } else {
+        setSuccess(SignUpPassword);
+    }
+
+    if (UserNameValue !== "" && SignUpEmailValue !== "" && SignUpPasswordValue !== "") {
+        window.location.href = "index.html";
+    }
+};
+
+
+// sign in form validation
+
+SignInForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        checkSignUpInputs();
+    }
+
+);
+
+let checkSignUpInputs = () => {
+    let SignInEmailValue = SignInEmail.value.trim();
+    let SignInPasswordValue = SignInPassword.value.trim();
+
+    if (SignInEmailValue === "") {
+        setError(SignInEmail, "Email cannot be blank");
+    } else if (!emailRegex.test(SignInEmailValue)) {
+        setError(SignInEmail, "Email is not valid");
+    } else {
+        setSuccess(SignInEmail);
+    }
+
+    if (SignInPasswordValue === "") {
+        setError(SignInPassword, "Password cannot be blank");
+    } else if (!passwordRegex.test(SignInPasswordValue)) {
+        setError(SignInPassword, "Password is not valid");
+    } else if (SignInPasswordValue.length < 8) {
+        setError(SignInPassword, "Password must be 8 characters long");
+    } else {
+        setSuccess(SignInPassword);
+    }
+
+    if (SignInEmailValue !== "" && SignInPasswordValue !== "") {
+        window.location.href = "index.html";
+    }
+};
